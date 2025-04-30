c48668b7a9e25861e5dcbd7101b8f2807490b786
Anthonywilliam0102 <anthonymotaw01@gmail.com>
2025-04-29 22:12:29 -0300

    Operação multiplicação

---
+++ multi.py
@@ -0,0 +1,8 @@
+def multiplicacao(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    print(f"A multiplicação é: {multiplicacao(num1, num2)}")
+
